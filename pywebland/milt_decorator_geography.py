3a697ca7fe422275e56417fe3c6ca73f7f5a0d5f
yosukesan <y.otsuki30@gmail.com>
2018-12-14 22:12:46 +0900

    modified: fixed to be strategy pattern.

--- pywebland/milt_decorator_geography.py
+++ pywebland/milt_decorator_geography.py
@@ -16,149 +16,31 @@ class DecoratorGeography:
 		return self._concrete_geography.city(code_area)	
 
 	def area(self):
-		"""
-		Description
-			returns area code from 1 to 47
-		"""
-
-		areas = {}
-
-		areas.update(self.hokkaido())
-		areas.update(self.tohoku())
-		areas.update(self.kanto())
-		areas.update(self.chubu())
-		areas.update(self.kansai())
-		areas.update(self.chugoku())
-		areas.update(self.shikoku())
-		areas.update(self.kyushu())
-		areas.update(self.okinawa())
-
-		return areas
+		return self._concrete_geography.area()
 
 	def hokkaido(self):
-		"""
-		Description
-			returns area code in hokkaido
-		"""
-
-		areas = {	'hokkaido'	: '01'}
-
-		return areas
+		return self._concrete_geography.hokkaido()
 
 	def tohoku(self):
-		"""
-		Description
-			returns area code in tohoku
-		"""
-		 
-		areas = {	 'aomori'	: '02',	
-				 	'iwate'		: '03',
-				 	'miyagi'	: '04',
-				 	'akita'		: '05',
-				 	'yamagata'	: '06',
-				 	'fukushima'	: '07'}
-		return areas
+		return self._concrete_geography.tohoku()
 
 	def kanto(self):
-		"""
-		Description
-			returns area code in kando
-		"""
-
-		areas = {	'ibaraki'	: '08',  
-				 	'tochigi'	: '09',	
-				 	'gumma'		: '10',	
-				 	'saitama'	: '11',
-				 	'chiba'		: '12',
-				 	'toyko'		: '13',
-				 	'kanagawa'	: '14'}
-
-		return areas
+		return self._concrete_geography.kanto()
 
 	def chubu(self):
-		"""
-		Description
-			returns area code in chubu
-		"""
-
-		areas = {	'nigata'	: '15',  
-					'toyama'	: '16',  
-					'ishikawa'	: '17',  
-					'fukui'		: '18',
-					'yamanashi'	: '19',
-					'nagano'	: '20',
-					'gifu'		: '21',
-					'shizuoka'	: '22',
-					'aichi'		: '23',
-					'mie'		: '24'}
-
-		return areas
+		return self._concrete_geography.chubu()
 
 	def kansai(self):
-		"""
-		Description
-			returns area code in kansai
-		"""
-
-		areas = {	'shiga'		: '25',  
-					'kyoyo'		: '26',  
-					'osaka'		: '27',  
-					'hyogo'		: '28',  
-					'nara'		: '29',  
-					'wakayama'	: '30'}  
-		return areas
+		return self._concrete_geography.kansai()
 
 	def chugoku(self):
-		"""
-		Description
-			returns area code in chugoku
-		"""
-
-		areas = {	'tottori'	: '31',  
-					'shimane'	: '32',  
-					'okayama'	: '33',  
-					'hiroshima'	: '34',  
-					'yamaguchi'	: '35'}
-
-		return areas
+		return self._concrete_geography.chugoku()
 
 	def shikoku(self):
-		"""
-		Description
-			returns area code in shikoku
-		"""
-
-		areas = {	'tokushima'	: '36',  
-					'kagawa'	: '37',  
-					'ehime'		: '38',  
-					'kochi'		: '39'} 
-
-		return areas
+		return self._concrete_geography.shikoku()
 
 	def kyushu(self):
-		"""
-		Description
-			returns area code in kyushu
-		"""
-
-		areas = {	'fukuoka'	: '40',  
-					'saga'		: '41',  
-					'nagasaki'	: '42',  
-					'kumamoto'	: '43',  
-					'oita'		: '44',  
-					'miyazaki'	: '45',  
-					'kagoshima'	: '46'} 
-		return areas
+		return self._concrete_geography.kyushu()
 
 	def okinawa(self):
-		"""
-		Description
-			returns area code in okinawa
-		"""
-
-		areas = {	'hokkaido'	: '01'}
-
-		return areas
-		
-
-
+		return self._concrete_geography.okinawa()
